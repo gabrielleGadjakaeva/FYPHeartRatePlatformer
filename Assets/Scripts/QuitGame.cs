737c79c58068a379b1038d46cd32e7ce71d4ec13
gabrielleGadjakaeva <gabrielle.gadjakaeva@gmail.com>
2025-05-03 19:22:05 +0100

    Final tweaks

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+// Script for quitting the game
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+}
